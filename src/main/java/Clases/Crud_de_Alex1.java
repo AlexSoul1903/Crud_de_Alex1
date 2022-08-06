e211c461bfd674494fb05c8360c0980a43be7a28
Alex Manuel Frias Molina <alexmanuel19frias@gmail.com>
2022-08-06 13:26:33 -0400

    Se agrega el proyecto

---
+++ src/main/java/Clases/Crud_de_Alex1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package Clases;
+
+import Forms.Login;
+
+/**
+ *
+ * @author alexm
+ */
+public class Crud_de_Alex1 {
+
+    public static void main(String[] args) {
+      
+        Login log = new Login();
+        
+       log.setVisible(true);
+        
+    }
+}
